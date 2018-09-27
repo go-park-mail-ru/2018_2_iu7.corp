ce71fb5f67593e0e00de287c38a1f7d1067b770c
denis96z <zinoviev.denis@yandex.ru>
2018-09-28 00:04:40 +0300

    Refactoring

--- profileRepository.go
+++ profileRepository.go
@@ -1,7 +1,5 @@
 package main
 
-import "sync"
-
 type ProfileRepository interface {
 	SaveNew(p Profile) (id uint64, err error)
 	SaveExisting(p Profile) (err error)
@@ -10,33 +8,4 @@ type ProfileRepository interface {
 
 	FindByID(id uint64) (p Profile, err error)
 	FindByUsernameAndPassword(username, password string) (p Profile, err error)
-}
-
-type InMemoryProfileRepository struct {
-	storage []Profile
-	rwMutex *sync.RWMutex
-}
-
-func NewInMemoryProfileRepository() *InMemoryProfileRepository {
-	return &InMemoryProfileRepository{}
-}
-
-func (r *InMemoryProfileRepository) SaveNew(p Profile) (id uint64, err error) {
-	panic("Not implemented yet!")
-}
-
-func (r *InMemoryProfileRepository) SaveExisting(p Profile) (err error) {
-	panic("Not implemented yet!")
-}
-
-func (r *InMemoryProfileRepository) DeleteByID(id uint64) (err error) {
-	panic("Not implemented yet!")
-}
-
-func (r *InMemoryProfileRepository) FindByID(id uint64) (p Profile, err error) {
-	panic("Not implemented yet!")
-}
-
-func (r *InMemoryProfileRepository) FindByUsernameAndPassword(username, password string) (p Profile, err error) {
-	panic("Not implemented yet!")
 }
---
+++ profileRepositoryInMemory.go
@@ -0,0 +1,32 @@
+package main
+
+import "sync"
+
+type InMemoryProfileRepository struct {
+	storage []Profile
+	rwMutex *sync.RWMutex
+}
+
+func NewInMemoryProfileRepository() *InMemoryProfileRepository {
+	return &InMemoryProfileRepository{}
+}
+
+func (r *InMemoryProfileRepository) SaveNew(p Profile) (id uint64, err error) {
+	panic("Not implemented yet!")
+}
+
+func (r *InMemoryProfileRepository) SaveExisting(p Profile) (err error) {
+	panic("Not implemented yet!")
+}
+
+func (r *InMemoryProfileRepository) DeleteByID(id uint64) (err error) {
+	panic("Not implemented yet!")
+}
+
+func (r *InMemoryProfileRepository) FindByID(id uint64) (p Profile, err error) {
+	panic("Not implemented yet!")
+}
+
+func (r *InMemoryProfileRepository) FindByUsernameAndPassword(username, password string) (p Profile, err error) {
+	panic("Not implemented yet!")
+}
