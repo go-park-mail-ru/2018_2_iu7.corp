789e4ff3cfa7f05f69fc4116180eca570d65e041
denis96z <zinoviev.denis@yandex.ru>
2018-10-13 15:52:02 +0300

    Script to make all services added

---
+++ make.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install protoc
+curl -L https://github.com/google/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_64.zip -o /tmp/protoc.zip
+unzip /tmp/protoc.zip -d "$HOME"/protoc
+mkdir -p "$HOME"/src && ln -s "$HOME"/protoc "$HOME"/src/protobuf
+
+# Install easyjson
+go get -v github.com/mailru/easyjson/...
+
+# Make services
+for d in ./*
+do
+  if [ -d "${d}" ]
+  then
+    cd ${d}
+    service_name="${d:2}"
+    if [ -f "./Makefile" ]
+    then
+      echo -e "${service_name}:\n"
+      make build && make test && make image
+      echo -e "\n"
+    fi
+    cd ./..
+  fi
+done
