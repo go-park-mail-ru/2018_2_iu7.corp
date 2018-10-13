f1cb056bb6e727658bb8354257fd49a5117108e6
denis96z <zinoviev.denis@yandex.ru>
2018-10-13 18:01:29 +0300

    make.sh modified

--- make.sh
+++ make.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Install protoc
+
+# Install protobuf
 curl -L https://github.com/google/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_64.zip -o /tmp/protoc.zip
-unzip /tmp/protoc.zip -d "$HOME"/protoc
-mkdir -p "$HOME"/src && ln -s "$HOME"/protoc "$HOME"/src/protobuf
+unzip /tmp/protoc.zip -d /tmp/protoc
+PATH="/tmp/protoc/bin:${PATH}"
+
+go get -v github.com/micro/protoc-gen-micro
+go get -v github.com/golang/protobuf/protoc-gen-go
+
 
 # Install easyjson
 go get -v github.com/mailru/easyjson/...
+
 
 # Make services
 for d in ./*
